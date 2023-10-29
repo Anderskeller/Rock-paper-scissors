e667418d35a070284af47da74adecb30c3f55c1c
Anderskeller <anderskeller00@gmail.com>
2023-10-29 21:23:17 +0100

    Comments on code

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-//
-
+// Get references to HTML elements
 let userScore = 0
 let computerScore = 0
 const userScore_span = document.getElementById("user-score");
@@ -10,28 +9,32 @@ const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 
+// Function to randomly select the computers choice
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
+
+// function to handle a win
 function win() {
     userScore++;
     userScore_span.textContent = userScore;
     computerScore_span.textContent = computerScore;
     result_div.textContent = "You win!";
 }
-
+// Function to handle a loss
 function lose() {
     computerScore++;
     userScore_span.textContent = userScore;
     computerScore_span.textContent = computerScore;
     result_div.textContent = "You lose!";
 }
-
+// Function to handle a draw
 function draw() {
     result_div.textContent = "It's a draw!";
 }
+// Function to play the game based on the choice of the user
 function game(userChoice) {
     const computerChoice = getComputerChoice();
     switch (userChoice + " " + computerChoice) {
@@ -55,6 +58,7 @@ function game(userChoice) {
             break;
     }
 }
+// Function to call the event listeners
 
     function main() {
         rock_div.addEventListener('click', function () {
@@ -69,5 +73,5 @@ function game(userChoice) {
             game('scissors');
         })
     }
-
+// Call the function so i can setup the game
     main();
